9d86408ad359485068142ed03f9dc41296fa51f4
David <david.a.davis@live.com>
2015-09-30 09:22:50 -0500

    Not running on the server, working on the issue.

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+//var connectionString = process.env.MONGOLAB_URI || 'localhost/'
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
