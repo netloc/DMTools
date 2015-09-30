20a53c197ee1a3ebc66e0e3da18348589170e47b
David <david.a.davis@live.com>
2015-09-30 11:03:39 -0500

    Modified app.js to listen on Heroku port, testing now.

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var app = express();
 
 //var connectionString = process.env.MONGOLAB_URI || 'localhost/'
 
+
+var port = process.env.port;
+app.listen(port);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
